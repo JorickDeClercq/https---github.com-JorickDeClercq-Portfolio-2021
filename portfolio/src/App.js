703430b3109e7120e6a76137b44a1ea6b4886f7d
Jorick De Clercq <jorick.de.clercq@student.ehb.be>
2021-10-28 19:44:12 +0200

    working on navigation

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,11 +1,17 @@
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-      </header>
+      <div className="header">
+        {/** Logo here */}
+        <h1 className="header__logo">Jorick.</h1>
+        {/** Navigation here */}
+        <div className="header__navigation">
+          <a href="#" className="item">Projects</a>
+          <a href="#" className="item">About</a>
+        </div>
+      </div>
     </div>
   );
 }
